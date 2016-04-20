0422606e14de3bece29bef752914ea235d3e7a22
Cade Colvin <cade.colvin@gmail.com>
2016-04-19 22:39:26 -0500

    Added skeleton classes for core package.

---
+++ src/core/ingredient.py
@@ -0,0 +1,9 @@
+class Ingredient:
+    name = ""
+    unit = ""
+    amount = 0
+
+    def __init__(self, name, unit, amount):
+        self.name = name
+        self.unit = unit
+        self.amount = amount
---
+++ src/core/recipe.py
@@ -0,0 +1,12 @@
+class Recipe:
+
+    ingredients = list()
+    directions = ""
+
+    def __init__(self, recipe_file):
+
+    def get_ingredients_from_file(self, recipe_file):
+
+    def get_directions_from_file(self, recipe_file):
+
+    def write_to_file(self, recipe_file):
