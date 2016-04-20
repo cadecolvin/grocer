b50adc8304896abd6aa5091eb759b06e88b7d7b3
cade.colvin@intelligentinsites.com <cade.colvin@intelligentinsites.com>
2016-04-20 16:39:06 -0500

    Blue Apron scraper now pulls ingredients from a url

--- grocer/finder/blue_apron_scraper.py
+++ grocer/finder/blue_apron_scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from grocer.core.ingredient import *
 
 ingredients = list()
 
@@ -11,4 +12,7 @@ raw_soup = BeautifulSoup(response.text)
 ingredients_element = raw_soup.find_all("li", attrs={"itemprop" : "ingredients"})
 
 for ingredient_element in ingredients_element:
-    ingredients.append(new Ingredient(ingredient_element))
+    print(ingredient_element.contents[1].contents[1].text)
+    print(ingredient_element.contents[1].contents[2])
+
+
