2f56a9df488eb8566da3f02fedf98bb9a468f374
Cade Colvin <cade.colvin@gmail.com>
2017-04-06 16:05:52 -0500

    Began refactoring for SQLAlchemy best practices

--- grocer/__init__.py
+++ grocer/__init__.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask import render_template
-from flask.ext.bcrypt import Bcrypt
-
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('config')
-app.config.from_pyfile('config.py')
+from config import config
+
 
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    db.init_app(app)
+    login_manager.init_app(app)
