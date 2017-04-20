881b8a4070624c68b9ce49a64aa03cd1b6390d22
Cade Colvin <cade.colvin@gmail.com>
2017-04-20 08:24:21 -0500

    Began adding the admin Blueprint

---
+++ grocer/admin/views.py
@@ -0,0 +1,12 @@
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_required, current_user
+from . import admin
+from ..import db
+from ..models import User, Ingredient, Measurement
+
+
+@admin.route('/ingredients' methods=['GET', 'POST'])
+def ingredients():
+    ingredient_list = Ingredient.query.all()
+    return render_template('admin/ingredients.html',
+            ingredients=ingredient_list)
